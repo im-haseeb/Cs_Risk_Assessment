e1c4f7f93a6df7bdedb8d2bda241e7323ce6bfc2
return-hello <haseeb.has96@gmail.com>
2024-05-23 16:13:35 +0500

    WIP

--- Cs_Risk_Assessment/Controllers/AssessmentController.cs
+++ Cs_Risk_Assessment/Controllers/AssessmentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cs_Risk_Assessment.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cs_Risk_Assessment.Controllers
 {
@@ -8,5 +9,33 @@ namespace Cs_Risk_Assessment.Controllers
 		{
 			return View();
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> Step2([FromForm] IFormCollection formData)
+		{
+			//return Json(formData);
+			List<AssetsViewModel> assetsList = new List<AssetsViewModel>();
+			if(formData.Any())
+			{
+				if(formData.Count > 1)
+				{
+					for(int i=0; i<=formData.First().Value.Count()-1; i++)
+					{
+						AssetsViewModel assetsViewModel = new AssetsViewModel
+						{
+							AssetName = formData["AssetName"][i],
+							AssetType = formData["AssetType"][i],
+							Reference = formData["Reference"][i],
+							Location = formData["Location"][i],
+							Owner = formData["Owner"][i],
+						};
+						assetsList.Add(assetsViewModel);
+					}
+					return View(assetsList);
+				}
+			}
+			return Json("Data not Found");
+		}
 	}
+
 }
---
+++ Cs_Risk_Assessment/ViewModels/AssetsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cs_Risk_Assessment.ViewModels
+{
+	public class AssetsViewModel
+	{
+        public string AssetName { get; set; }
+        public string AssetType { get; set; }
+        public string Reference { get; set; }
+        public string Location { get; set; }
+        public string Owner { get; set; }
+    }
+}
