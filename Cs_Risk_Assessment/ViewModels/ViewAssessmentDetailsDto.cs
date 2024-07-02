d7762a1d0e50388e37fb9b9856385b05c102ce3e
return-hello <haseeb.has96@gmail.com>
2024-07-02 11:40:09 +0500

    WIP

---
+++ Cs_Risk_Assessment/ViewModels/ViewAssessmentDetailsDto.cs
@@ -0,0 +1,37 @@
+﻿namespace Cs_Risk_Assessment.ViewModels
+{
+    public class ViewAssessmentDetailsDto
+    {
+        public int ThreatsCount { get; set; }
+        public Top5Risks? Top5Risks { get; set; }
+        public Top5Vul? Top5Vul { get; set; }
+    }
+
+    public class Top5Vul
+    {
+        public string name1 { get; set; }
+        public int usage1 { get; set; }
+        public string name2 { get; set; }
+        public int usage2 { get; set; }
+        public string name3 { get; set; }
+        public int usage3 { get; set; }
+        public string name4 { get; set; }
+        public int usage4 { get; set; }
+        public string name5 { get; set; }
+        public int usage5 { get; set; }
+    }
+
+    public class Top5Risks
+    {
+        public string name1 { get; set; }
+        public int usage1 { get; set; }
+        public string name2 { get; set; }
+        public int usage2 { get; set; }
+        public string name3 { get; set; }
+        public int usage3 { get; set; }
+        public string name4 { get; set; }
+        public int usage4 { get; set; }
+        public string name5 { get; set; }
+        public int usage5 { get; set; }
+    }
+}
